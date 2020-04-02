6b39e7c5db0ec602b93b8b35a54e8248e335660e
Frederik Lyhne Andersen <freddybla17@gmail.com>
2020-04-02 13:44:10 +0200

    added comments

--- Interpret.fs
+++ Interpret.fs
@@ -2,27 +2,34 @@ module Interpret
 
 open System.Collections.Generic
 
+//The interpreter keeps track of two Edge lists. The first one never changes and are all possible edges
+//in the program. The second edge list is used for searching for an edge with the desired start node and
+//is shortened until either an fitting edge is found or the list is empty and the program fails.
 let rec interpret (Node(s)) (edgeList: Edge list) (remain: Edge list) (mem: Map<string, int>) =
     match remain with
-    |[] -> raise (System.InvalidOperationException("failed in interpret"))
+    |[] -> raise (System.InvalidOperationException("failed in interpret")) //This should never happen
+    |[Edge(Node(s1), logic, Node(s2))] when s <> s1 -> s, mem //The last edge doesn't have the desired start node
     |Edge(Node(s1), logic, Node(s2))::res when s = s1 -> try
                                                              let evalresult = evaluateEdge logic mem
                                                              match evalresult with 
-                                                                | Some(memN,true) -> if s2 = "qE" then
+                                                                //Case where the edge is accepted
+                                                                | Some(memN,true) -> if s2 = "qE" then 
                                                                                         s2, memN 
                                                                                      else   
                                                                                         interpret (Node(s2)) edgeList edgeList memN
+                                                                //Case where edge is rejected(boolean condition doesn't hold)
                                                                 | Some(memN,false) -> interpret (Node(s1)) edgeList res memN
+                                                                //Altering a variable that hasn't been initialized
                                                                 | None -> s1, mem
-                                                         with | :? KeyNotFoundException -> s1, mem
+                                                         with | :? KeyNotFoundException -> s1, mem //Accessing a variable that hasn't been initialized
     | _::res -> interpret (Node(s)) edgeList res mem
  
 and evaluateEdge (logic: Logic) (mem: Map<string, int>) =
     match logic with
-        |AssignE(x, a) -> let value = evalArithExpr a mem
-                          if mem.ContainsKey x then
+        | AssignE(x, a) -> let value = evalArithExpr a mem
+                           if mem.ContainsKey x then //Can't alter a variable that hasnt't been initialized
                                 Some(mem.Add(x, value), true)
-                          else 
+                           else 
                                 None
         | ArrayAssignE(x, a1, a2) ->  let index = evalArithExpr a1 mem
                                       let value = evalArithExpr a2 mem
@@ -57,10 +64,10 @@ and evalBoolExpr b (mem: Map<string, int>) : bool =
         | SOr (b1 , b2) -> (evalBoolExpr b1 mem) || (evalBoolExpr b2 mem)
         | And (b1 , b2) -> let b1res = evalBoolExpr b1 mem
                            let b2res = evalBoolExpr b2 mem
-                           b1res && b2res
+                           (b1res && b2res) || (b2res && b1res) //Forces both expressions to be evaluated
         | Or (b1 , b2) -> let b1res = evalBoolExpr b1 mem
                           let b2res = evalBoolExpr b2 mem
-                          b1res || b2res
+                          (b1res || b2res) && (b2res || b1res) //Forces both expressions to be evaluated
         | Not (b) ->  not (evalBoolExpr b mem)
         | Gt (a1, a2) -> (evalArithExpr a1 mem) > (evalArithExpr a2 mem)
         | Lt (a1, a2) -> (evalArithExpr a1 mem) < (evalArithExpr a2 mem) 
